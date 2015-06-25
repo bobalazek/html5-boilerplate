ebc91b11892a7c64f930e50c5c48ac0e9d57f24a
Borut Balažek <bobalazek124@gmail.com>
2015-06-25 22:44:53 +0200

    Text fix

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -4,12 +4,9 @@ var Application = function () {
 	return {
 		initialize: function()
 		{
-			// Some stuff here
 			jQuery(document).ready( function() {
-                // Do stuff here...
-                
                 jQuery('#preloader').fadeOut(); // Hide preloader, when everything is ready...
-
+                
                 initialized = true;
                 console.log('Application Initialized');
             });
